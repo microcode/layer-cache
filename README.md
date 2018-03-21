de490a9de791b82c3e262689bb09edf4d770b075
Jesper Svennevid <jesper.svennevid@dice.se>
2018-03-21 16:18:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# layer-cache
+
+Simple layered caching for Node.
+
+## Getting Started
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/microcode/layer-cache/tags). 
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
---
+++ test/onion.js
@@ -0,0 +1,86 @@
+const { OnionCache } = require('../lib/onion');
+const { MemoryCache } = require('../lib/memory');
+
+const assert = require('assert');
+
+describe('OnionCache', function () {
+    describe('get', function () {
+        it('should fetch from first cache', function () {
+            const fastCache = new MemoryCache();
+            const slowCache = new MemoryCache();
+            const cache = new OnionCache(fastCache, slowCache);
+            const testKey = 'key';
+            const testValue = 'value';
+
+            return fastCache.set(testKey, testValue).then(function () {
+                cache.get(testKey).then((result) => {
+                    assert(result === testValue);
+                })
+            });
+        });
+
+        it('should fetch from second cache if not in first cache', function () {
+            const fastCache = new MemoryCache();
+            const slowCache = new MemoryCache();
+            const cache = new OnionCache(fastCache, slowCache);
+            const testKey = 'key';
+            const testValue = 'value';
+
+            return slowCache.set(testKey, testValue).then(function () {
+                cache.get(testKey).then((result) => {
+                    assert(result === testValue);
+                })
+            });
+        });
+    });
+
+    describe('set', function () {
+        it('should set to all caches', function () {
+            const fastCache = new MemoryCache();
+            const slowCache = new MemoryCache();
+            const cache = new OnionCache(fastCache, slowCache);
+            const testKey = 'key';
+            const testValue = 'value';
+
+            return cache.set(testKey, testValue).then(() => {
+                return fastCache.get(testKey).then((result) => {
+                    assert(result === testValue);
+                });
+            }).then(() => {
+                return slowCache.get(testKey).then((result) => {
+                    assert(result === testValue);
+                });
+            });
+        });
+    });
+
+    describe('del', function () {
+        it('should delete from all caches', function () {
+            const fastCache = new MemoryCache();
+            const slowCache = new MemoryCache();
+            const cache = new OnionCache(fastCache, slowCache);
+            const testKey = 'key';
+            const testValue = 'value';
+
+            return cache.set(testKey, testValue).then(() => {
+                return fastCache.get(testKey).then((result) => {
+                    assert(result === testValue);
+                });
+            }).then(() => {
+                return slowCache.get(testKey).then((result) => {
+                    assert(result === testValue);
+                });
+            }).then(() => {
+                return cache.del(testKey);
+            }).then(() => {
+                return fastCache.get(testKey).then((result) => {
+                    assert(result === undefined);
+                });
+            }).then(() => {
+                return slowCache.get(testKey).then((result) => {
+                    assert(result === undefined);
+                });
+            });
+        });
+    })
+});
