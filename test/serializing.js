543b8f81d98638e8c6d09f5a9f11ca09b9850200
Jesper Svennevid <jsvennevid@users.noreply.github.com>
2018-03-24 11:11:11 +0100

    Added NonSerializingCache (if you want the non-serializing version of the cache fill)

--- test/serializing.js
+++ test/serializing.js
@@ -1,5 +1,5 @@
 const { MemoryCache } = require('..');
-const { SerializingCache } = require('..');
+const { SerializingCache, NonSerializingCache } = require('..');
 const { DelayedCache } = require('./util/delayed');
 
 const assert = require('assert');
@@ -109,6 +109,116 @@ describe("SerializingCache", function () {
                 cache.del(testKey), cache.del(testKey), cache.del(testKey)
             ]).then(() => {
                 assert.equal(rootCache.cache.counter, 2, "Cache entry should be deleted twice");
+            });
+        });
+    });
+});
+
+describe("NonSerializingCache", function () {
+    describe('get', function () {
+        it ("should fill using callback", function () {
+            const testValue = 'value';
+            const testKey = 'key';
+
+            const memoryCache = new MemoryCache();
+            const cache = new NonSerializingCache(memoryCache, () => {
+                return new Promise((resolve) => resolve(testValue));
+            });
+
+            return cache.get(testKey).then((result) => {
+                assert(result === testValue);
+            }).then(() => {
+                return memoryCache.get(testKey).then((result) => {
+                    assert(result === testValue);
+                })
+            });
+        });
+
+        it("should only issue one read per request run", function () {
+            class TestCache extends MemoryCache {
+                constructor() {
+                    super();
+                    this.counter = 0;
+                }
+                get(key) {
+                    this.counter++;
+                    return super.get(key);
+                }
+            }
+
+            const testValue = 'value';
+            const testKey = 'key';
+
+            const rootCache = new DelayedCache(new TestCache(), { get: 10 });
+            const cache = new NonSerializingCache(rootCache, () => {
+                return new Promise((resolve) => resolve(testValue));
+            });
+
+            return Promise.all([
+                cache.get(testKey), cache.get(testKey), cache.get(testKey)
+            ]).then(() => {
+                assert.equal(rootCache.cache.counter, 3, "Cache should be read three times");
+            });
+        });
+    });
+
+    describe('set', function () {
+        it('should not be supported', function () {
+            const memoryCache = new MemoryCache();
+            const cache = new NonSerializingCache(memoryCache, () => {
+                return new Promise((resolve) => resolve("value"));
+            });
+            const testKey = 'key';
+
+            return cache.set(testKey, "dummy").then(() => {
+                throw Error("Set successful");
+            }, (err) => {});
+        });
+    });
+
+    describe('del', function () {
+        it ("should fill using callback", function () {
+            const testValue = 'value';
+            const testKey = 'key';
+
+            const memoryCache = new MemoryCache();
+            const cache = new NonSerializingCache(memoryCache, () => {
+                return new Promise((resolve) => resolve(testValue));
+            });
+
+            return cache.get(testKey).then((result) => {
+                assert(result === testValue);
+            }).then(() => {
+                return memoryCache.get(testKey).then((result) => {
+                    assert(result === testValue);
+                })
+            });
+        });
+
+        it("should only issue one delete per request run", function () {
+            class TestCache extends MemoryCache {
+                constructor() {
+                    super();
+                    this.counter = 0;
+                }
+                del(key) {
+                    this.counter++;
+                    return super.del(key);
+                }
+            }
+
+            const testValue = 'value';
+            const testKey = 'key';
+
+            const rootCache = new DelayedCache(new TestCache(), { get: 10 });
+            const cache = new NonSerializingCache(rootCache, () => {
+                return new Promise((resolve) => resolve(testValue));
+            });
+
+            return Promise.all([
+                cache.del(testKey), cache.del(testKey), cache.del(testKey)
+            ]).then(() => {
+                assert.equal(rootCache.cache.counter, 3, "Cache entry should be deleted three times");
             });
         });
     });
