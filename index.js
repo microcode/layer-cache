5c72dabb8d1c56faba0957b32498f54851fbd43a
Jesper Svennevid <jsvennevid@users.noreply.github.com>
2018-03-24 11:11:11 +0100

    Added NonSerializingCache (if you want the non-serializing version of the cache fill)

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const { BaseCache } = require('./lib/base');
-const { SerializingCache } = require('./lib/serializing');
+const { SerializingCache, NonSerializingCache } = require('./lib/serializing');
 const { MemoryCache } = require('./lib/memory');
 const { OnionCache } = require('./lib/onion');
 
 exports.BaseCache = BaseCache;
 exports.SerializingCache = SerializingCache;
+exports.NonSerializingCache = NonSerializingCache;
 exports.MemoryCache = MemoryCache;
 exports.OnionCache = OnionCache;
